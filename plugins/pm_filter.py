520d5e7f4d60d620fe5e08f2fd5153feb329f65e
sahara81 <saitama36900@gmail.com>
2025-12-01 02:28:57 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -2051,6 +2051,7 @@ async def auto_filter(client, msg, spoll=False):
             search = search.replace("-", " ")
             search = search.replace(":","")
             files, offset, total_results = await get_search_results(message.chat.id ,search.lower(), offset=0, filter=True)
+            files = await sort_results_for_user(message.from_user.id, files)
             if not files:
                 await m.delete()
                 if settings["spell_check"]:
