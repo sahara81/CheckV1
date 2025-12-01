c970ec818f40b5d17e2e12fda23f5d3119f9bb20
sahara81 <saitama36900@gmail.com>
2025-12-01 18:12:37 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -358,6 +358,14 @@ async def next_page(bot, query):
         InlineKeyboardButton("Lᴀɴɢᴜᴀɢᴇ", callback_data=f"select_lang#{req}"),
         InlineKeyboardButton("Sᴇᴀꜱᴏɴꜱ", callback_data=f"jk_dev#{req}")
     ])
+    # 📂 EPISODE GRID BUTTON FIXED POSITION
+    if re.search(r"S\d{1,2}", movie, flags=re.IGNORECASE) or "season" in movie.lower():
+    btn.insert(0, [
+        InlineKeyboardButton(
+            "📂 Episodes Grid",
+            callback_data=f"epgrid#{userid}#{movie}"
+        )
+    ])
     btn.insert(0, [
         InlineKeyboardButton("📥 𝗦𝗲𝗻𝗱 𝗔𝗹𝗹 𝗙𝗶𝗹𝗲𝘀 📥", callback_data=f"send_fall#files#{offset}#{req}"),
     ])
@@ -431,10 +439,10 @@ async def language_check(bot, query):
             InlineKeyboardButton("Sᴇʟᴇᴄᴛ Aɢᴀɪɴ", callback_data=f"select_lang#{userid}"),
             InlineKeyboardButton("Sᴇᴀꜱᴏɴꜱ", callback_data=f"jk_dev#{userid}")
         ])
-# 📂 EPISODE GRID BUTTON FIXED POSITION
-if re.search(r"S\d{1,2}", movie, flags=re.IGNORECASE) or "season" in movie.lower():
-    btn.insert(0, [
-        InlineKeyboardButton(
+        # 📂 EPISODE GRID BUTTON FIXED POSITION
+        if re.search(r"S\d{1,2}", movie, flags=re.IGNORECASE) or "season" in movie.lower():
+        btn.insert(0, [
+            InlineKeyboardButton(
             "📂 Episodes Grid",
             callback_data=f"epgrid#{userid}#{movie}"
         )
@@ -569,14 +577,14 @@ async def quality_check(bot, query):
             InlineKeyboardButton("Lᴀɴɢᴜᴀɢᴇ", callback_data=f"select_lang#{userid}"),
             InlineKeyboardButton("Sᴇᴀꜱᴏɴꜱ", callback_data=f"jk_dev#{userid}")
         ])
-# 📂 EPISODE GRID BUTTON FIXED POSITION
-if re.search(r"S\d{1,2}", movie, flags=re.IGNORECASE) or "season" in movie.lower():
-    btn.insert(0, [
-        InlineKeyboardButton(
+#       # 📂 EPISODE GRID BUTTON FIXED POSITION
+        if re.search(r"S\d{1,2}", movie, flags=re.IGNORECASE) or "season" in movie.lower():
+        btn.insert(0, [
+            InlineKeyboardButton(
             "📂 Episodes Grid",
             callback_data=f"epgrid#{userid}#{movie}"
         )
-    ])
+      ])
 
         btn.insert(0, [
             InlineKeyboardButton("📥 𝗦𝗲𝗻𝗱 𝗔𝗹𝗹 𝗙𝗶𝗹𝗲𝘀 📥", callback_data=f"send_fall#{pre}#{0}#{userid}")
